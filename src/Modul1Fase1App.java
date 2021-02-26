ae9ab4f491bb557d581c71b228254d328795f663
Lluis Gutierrez <betta_betta@hotmail.com>
2021-02-26 15:18:38 +0100

    Modul1Fase1

---
+++ src/Modul1Fase1App.java
@@ -0,0 +1,22 @@
+
+public class Modul1Fase1App {
+
+	
+		public static void main(String[] args) {
+			String nom="Lluis";
+			String cognom1="Gutiérrez";
+			String cognom2="López";
+			int dia=27;
+			int mes=11;
+			int any=1979;
+			
+			System.out.println(nom + " " + " " + cognom1 + " " + cognom2);
+			
+			System.out.println( dia + "/" + mes + "/" + any );
+			
+			
+			
+			
+		}
+
+	}
